5bcd237394235df7528a4e76da644a846fa2098a
Donghwa Shin <68676833+dhshin04@users.noreply.github.com>
2024-09-24 12:03:49 -0400

    Removed unnecessary function

--- bot.py
+++ bot.py
@@ -5,12 +5,12 @@ from weather import get_weather
 from config import TOKEN
 
 
-#Step 1: BOT SETUP
+# BOT SETUP
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
 
-#Step 2: Message Function
+# Message Function
 async def send_message(message: Message,user_message: str) -> None:
     # ---------------------------------------------------------------------
     # WRITE NEW RESPONSES HERE !!
@@ -31,17 +31,12 @@ async def send_message(message: Message,user_message: str) -> None:
         return
 
 
-def startWith(msg: str, prompt: str) -> bool:
-    return msg.lower().startswith(prompt)
-
-    # ---------------------------------------------------------------------
-
-#Step 3: Handle the startup of the bot
+# Handle the startup of the bot
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
 
-#Step 4:  Handles the messages: waits for a message to be sent, then calls "send_message()" to send a response
+# Handles the messages: waits for a message to be sent, then calls "send_message()" to send a response
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user: #The bot wrote the message, or the bot talks to itself
@@ -54,7 +49,7 @@ async def on_message(message: Message) -> None:
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
-#Step 5 Main Starting point
+# Main Starting point
 def main() -> None:
     client.run(token=TOKEN)
 
